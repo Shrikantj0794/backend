5bd357fa654860763b52a33e3ea3ba5201952dad
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-25 13:19:47 +0530

    Creating ThunderClient Collections to Manage Request

--- routes/auth.js
+++ routes/auth.js
@@ -3,25 +3,35 @@ const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-//Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', [ //adding validations
+//Create a User using: POST "/api/auth/createuser". NO login required
+router.post('/createuser', [ //adding validations
     body('name', 'Enter a valid name').isLength({ min:3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password','password must be atleast 5 char').isLength({ min:5 })
-],(req,res)=>{
+], async(req,res)=>{
     //msg for errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    //create user
-    User.create({
+    //check weather the user axist already 
+
+    try {
+      
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+      return res.status(400).json({error: "Sorry a user with this email already axist"})
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err)
-    res.json({errors: 'please enter a Unique value foe email'})})
+      })
+      res.json({"successfully": "successfully"})
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured")
+    }
 })
 
 module.exports = router
