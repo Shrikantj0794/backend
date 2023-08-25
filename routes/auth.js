b95873a6d0431004601aa27a0c0fc0a4e7d715be
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-25 11:34:39 +0530

    adding validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,19 @@
-const express = require('express')
+const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req,res)=>{
-    obj = {
-        a: 'thios',
-        number:34
+//Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post('/', [ //adding validations
+    body('name').isLength({ min:3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min:3 })
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(obj)
+    res.send(req.body)
 })
 
 module.exports = router
