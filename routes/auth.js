c8d15bd2fe166f3fa6cf1bf0d48d2ce528277dd1
Shrikant Jamale <shrikantj0794@gmail.com>
2023-08-25 11:45:33 +0530

    change errors msg

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,11 @@ const { body, validationResult } = require('express-validator');
 
 //Create a User using: POST "/api/auth/". Doesn't require Auth
 router.post('/', [ //adding validations
-    body('name').isLength({ min:3 }),
-    body('email').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min:3 }),
+    body('email', 'Enter a valid email').isEmail(),
     body('password').isLength({ min:3 })
 ],(req,res)=>{
+    //msg for errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
